d7541e90cce714ab743896eaaf7f5a07cf55081f
Basheer Subei <basheersubei@gmail.com>
2015-07-13 11:25:18 -0500

    repl loop now gathers input until %END is entered

--- repl_leaf/repl.tcl
+++ repl_leaf/repl.tcl
@@ -1,12 +1,28 @@
 package provide repl 0.0
+
+package require turbine 0.6.0
+namespace import turbine::*
+
 namespace eval repl {
 
     proc start_repl {} {
+	
+	puts "Welcome to the extremely basic REPL turbine!"
 
-	while "1" {
-	    set input [gets stdin]
-	    puts "$input"
+
+	# keep taking user input until %END is entered
+	set input [gets stdin]
+	puts "user input: $input"
+	# store entire input here (concatenate it)
+	set all_input ""
+
+	while {[string compare $input "%END"] != 0} {
+	    set all_input [concat $all_input $input]	    
+	    set input [gets stdin]	    
+	    puts "user input: $input" 
 	}
 
+	puts "finished!"
+	puts "all_input: $all_input"
     }
 }
