29b2f44c456bac4bc370606158317ac5345b14c9
Basheer Subei <basheersubei@gmail.com>
2015-07-22 14:26:42 -0500

    converts tabs to spaces, formatting fixes

--- repl_leaf/repl.tcl
+++ repl_leaf/repl.tcl
@@ -5,43 +5,42 @@ namespace import turbine::*
 
 namespace eval repl {
 
-    proc start_repl {} {
-	
-	puts "Welcome to the extremely basic REPL turbine!"
+  proc start_repl {} {
+
+    puts "Welcome to the extremely basic REPL turbine!"
 
 
-	# keep taking user input until %END is entered
-	set input [gets stdin]
-	# store entire input here (concatenate it)
-	set allInput ""
+    # keep taking user input until %END is entered
+    set input [gets stdin]
+    # store entire input here (concatenate it)
+    set allInput ""
 
-	while {[string compare $input "%END"] != 0} {
-	    append allInput "$input\n" 
-	    set input [gets stdin]	    
-	}
-
-	puts "finished!"
-	puts "Attempting to run user input script:\n$allInput"
-
-	# write input to swift file and compile it in STC
-    	set swiftFilename "tmp.swift"
-    	set fileId [open $swiftFilename "w"]
-    	puts -nonewline $fileId $allInput
-    	close $fileId
-	puts "Wrote tmp file to: $swiftFilename. Calling STC on it."
-	set ticFilename "tmp.tic"
-	exec stc -c -O0 -V $swiftFilename $ticFilename
-	# read in tic output
-	set fileId [open $ticFilename "r"]
-	set ticOutput [read $fileId]
-	close $fileId
-
-	# append main function call to script
-	append ticOutput "\nswift:main\n"
-
-	# now eval the tric code
-	# eval $ticOutput
-	uplevel #0 $ticOutput
+    while {[string compare $input "%END"] != 0} {
+      append allInput "$input\n"
+      set input [gets stdin]
     }
 
-}
+    puts "finished!"
+    puts "Attempting to run user input script:\n$allInput"
+
+    # write input to swift file and compile it in STC
+    set swiftFilename "tmp.swift"
+    set fileId [open $swiftFilename "w"]
+    puts -nonewline $fileId $allInput
+    close $fileId
+    puts "Wrote tmp file to: $swiftFilename. Calling STC on it."
+    set ticFilename "tmp.tic"
+    exec stc -c -O0 -V $swiftFilename $ticFilename
+    # read in tic output
+    set fileId [open $ticFilename "r"]
+    set ticOutput [read $fileId]
+    close $fileId
+
+    # append main function call to script
+    append ticOutput "\nswift:main\n"
+
+    # now eval the tric code
+    # eval $ticOutput
+    uplevel #0 $ticOutput
+
+  }
