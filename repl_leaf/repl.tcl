e14db3a56afb7ecc05c68688c14b25e97b32c7c3
Basheer Subei <basheersubei@gmail.com>
2015-07-22 14:20:21 -0500

    uses -c flag to remove boilerplate instead of doing it manually

--- repl_leaf/repl.tcl
+++ repl_leaf/repl.tcl
@@ -30,46 +30,20 @@ namespace eval repl {
     	close $fileId
 	puts "Wrote tmp file to: $swiftFilename. Calling STC on it."
 	set ticFilename "tmp.tic"
-	exec stc -V $swiftFilename $ticFilename
-	# read in tic output and process it (remove boilerplate code)
+	exec stc -c -O0 -V $swiftFilename $ticFilename
+	# read in tic output
 	set fileId [open $ticFilename "r"]
 	set ticOutput [read $fileId]
 	close $fileId
 
-	set finalTicOutput ""
-	set lines [split $ticOutput "\n"]
-
-	# remove all lines of code that start with any of these terms	
-	set searchTerms [list "package" "namespace" "turbine::defaults" "turbine::defaults" "turbine::declare_custom_work_types" "turbine::init" "turbine::enable_read_refcount" "turbine::check_constants" "turbine::finalize" "turbine::start" "adlb::declare_struct_type" "#*"]
-	set lineNumbersToRemove [list]
-	set lineNumber 0
-	# mark which lines of code start with these terms
-	foreach line $lines {
-	    foreach searchTerm $searchTerms {
-		if {[ string match $searchTerm* $line ]} {
-		    lappend lineNumbersToRemove $lineNumber
-		    break
-		}
-	    }
-	    set lineNumber [expr {$lineNumber+1}]
-	}
-
-	# now remove the marked lines
-	# iterate over list backwards (so removing lines doesn't change line numbers ahead of current point)
-	for {set i [expr {[llength $lineNumbersToRemove] - 1}]} {$i >= 0} {incr i -1} {
-	    set index [lindex $lineNumbersToRemove $i]
-	    set lines [lreplace $lines $index $index]
-	}
-	
 	# append main function call to script
-	lappend lines "swift:main\n"
-	# now join it into one string
-	set cleanTicOutput [join $lines \n]
+	append ticOutput "\nswift:main\n"
+
 	# write it to a file for debugging (the r in "tric" stands for REPL)	
 	set fileId [open "tmp.tric" "w"]
-    	puts -nonewline $fileId $cleanTicOutput
-    
-	
+	puts -nonewline $fileId $ticOutput
+	close $fileId
+
 	# now eval the tric code
 	eval $cleanTicOutput
     }
